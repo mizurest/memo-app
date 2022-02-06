ce9b474afd41b1821095fac9f4db26a98f43fd2c
ユーザー名 <hybs11@yahoo.co.jp>
2022-02-06 17:44:07 +0900

    新規作成と編集画面の作成

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 // import MemoListScreen from './src/screeens/MemoListScreen';
-import MemoDetailScreen from './src/screeens/MemoDetailScreen';
+// import MemoDetailScreen from './src/screeens/MemoDetailScreen';
+// import MemoEditScreen from './src/screeens/MemoEditScreen';
+import MemoCreateScreen from './src/screeens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoCreateScreen />
   );
 }
---
+++ src/screeens/MemoEditScreen.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  View, StyleSheet, TextInput, KeyboardAvoidingView,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoEditScreen() {
+  return (
+    <KeyboardAvoidingView behavior="height" style={styles.container}>
+
+      <AppBar />
+
+      <View style={styles.inputContainer}>
+        <TextInput value="買い物リスト" multiline style={styles.input} />
+      </View>
+
+      <CircleButton name="check" />
+
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  inputContainer: {
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+    flex: 1,
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 24,
+    flex: 1,
+    textAlignVertical: 'center',
+  },
+});
