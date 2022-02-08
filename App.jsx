26e6093702d62564bda6167f1e4d6040c6d48ab2
ユーザー名 <hybs11@yahoo.co.jp>
2022-02-08 23:10:36 +0900

    ユーザー登録機能の実装

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screeens/MemoListScreen';
 import MemoDetailScreen from './src/screeens/MemoDetailScreen';
@@ -10,6 +11,20 @@ import LoginScreen from './src/screeens/LoginScreen';
 import SignupScreen from './src/screeens/SignupScreen';
 
 const Stack = createStackNavigator();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBxmvw9UVH-IIV5xDX-Dnf8oQSpQXNHkgo',
+  authDomain: 'memoapp-8c86e.firebaseapp.com',
+  projectId: 'memoapp-8c86e',
+  storageBucket: 'memoapp-8c86e.appspot.com',
+  messagingSenderId: '10372382284',
+  appId: '1:10372382284:web:7cca7934a9fcb92960bee9',
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
--- src/screeens/SignupScreen.jsx
+++ src/screeens/SignupScreen.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import {
-  View, Text, TextInput, StyleSheet, TouchableOpacity,
+  View, Text, TextInput, StyleSheet, TouchableOpacity, Alert,
 } from 'react-native';
+import firebase from 'firebase';
 
 import Button from '../components/Button';
 
@@ -9,6 +10,23 @@ export default function SignupScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+
+  const handlePress = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const { user } = userCredential;
+        console.log(user.uid);
+
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      })
+      .catch((error) => {
+        Alert.alert(error.code);
+      });
+  };
+
   return (
     <View style={styles.container}>
 
@@ -35,12 +53,7 @@ export default function SignupScreen(props) {
 
         <Button
           label="Submit"
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'MemoList' }],
-            });
-          }}
+          onPress={handlePress}
         />
 
         <View style={styles.footerContainer}>
