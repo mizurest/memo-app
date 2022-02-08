48b8ce7d263aa7bbdcf01185477348744ea36e19
ユーザー名 <hybs11@yahoo.co.jp>
2022-02-09 08:10:29 +0900

    ログアウト機能の実装

---
+++ src/components/LogOutButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  StyleSheet, Text, TouchableOpacity, Alert,
+} from 'react-native';
+import firebase from 'firebase';
+import { useNavigation } from '@react-navigation/native';
+
+export default function LogOutButton() {
+  const navigation = useNavigation();
+  const handlePress = () => {
+    firebase.auth().signOut()
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'Login' }],
+        });
+      })
+      .catch(() => {
+        Alert.alert('ログアウトに失敗しました');
+      });
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      style={styles.container}
+    >
+      <Text style={styles.label}>ログアウト</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+  label: {
+    fontSize: 14,
+    color: 'rgba(255, 255, 255, 0.7)',
+  },
+});
